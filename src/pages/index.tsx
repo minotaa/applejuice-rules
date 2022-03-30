cf62ab9f44c9df360aad3ece6ffbc23e5776d28d
Juan <jpichardo369@gmail.com>
2022-03-30 18:07:00 -0400

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,62 @@
+import Head from "next/head"
+import Link from "next/link"
+
+export default function Home() {
+    return (
+        <>
+            <header>
+                <Head>
+                    <title>applejuice rules</title>
+                </Head>
+            </header>
+            <main>
+                <h1>&nbsp;🧃 applejuice rules</h1>
+                <h2>&nbsp;&nbsp;&nbsp;Join the discord server @ <Link href="https://discord.gg/X7ujJThANP">discord.gg/X7ujJThANP</Link>, appeal using this <Link href="https://discord.gg/X7ujJThANP">this form.</Link></h2>
+                <br/>
+                <h1>&nbsp;🎮 In-game Bans</h1>
+                <h2>&nbsp;&nbsp;&nbsp;The following can get you banned:</h2>
+                <ul>
+                    <li><strong>Usage of a Hacked Client</strong> - Using a hacked clinet.</li>
+                    <li><strong>Unfair Advantage</strong> - Knowingly obtaining items from teammates or F5 Abusing.</li>
+                    <li><strong>X-ray / Cavefinder</strong> - Using a mod or resource pack to help you find ores/caves more easier.</li>
+                    <li><strong>Extreme Disrespect</strong> - Extreme disrespect to players or staff.</li>
+                    <li><strong>Death Wishes/Threats</strong> - Wishing or threatening death upon someone.</li>
+                    <li><strong>Breaking Random Team Rules</strong> - Suiciding &amp; leaving the game during a random teams game qualifies as this.</li>
+                    <li><strong>DDoS-ing</strong> - Attempts to, or threats to get the server booted offline qualify as breaking this rule.</li>
+                    <li><strong>Dox-ing</strong> - Attempts to, or threats to reveal personal information about players qualify as breaking this rule.</li>
+                    <li><strong>iPvP</strong> - Attempting to damage players before PvP qualify as breaking this rule.</li>
+                    <li><strong>Not listening to staff</strong> - Not listening to staff despite receiving multiple warnings.</li>
+                    <li><strong>Stalking</strong> - Continuing to stalk/killing a player despite being warned not to.</li>
+                    <li><strong>Bug/Glitch Abuse</strong> - Abusing a bug/glitch in the server.</li>
+                    <li><strong>Extreme Spoiling</strong> - Spoiling crucial information, e.g. moles in a game.</li>
+                    <li><strong>Crossteaming</strong> - If not allowed, teaming with someone in a teams game or an FFA game qualifies as crossteaming.</li>
+                </ul>
+                <br/>
+                <h1>&nbsp;🤫 In-game Mutes</h1>
+                <h2>&nbsp;&nbsp;&nbsp;The following can get you muted:</h2>
+                <ul>
+                    <li><strong>Disrespect</strong> - Mild disrespect to players or staff.</li>
+                    <li><strong>Spoiling</strong> - Mild spoiling, e.g. revealing what other players have in terms of gear.</li>
+                    <li><strong>Spamming</strong> - Mild spamming, e.g. sending 3 of the same message within a short period of time.</li>
+                    <li><strong>Extreme Spamming</strong> - Extreme spamming, e.g. sending 10 of the same message within a short period of time.</li>
+                    <li><strong>Annoying</strong> - Being a bother to the players/host.</li>
+                    <li><strong>Spamming Help-Op</strong> - Abusing the /helpop system with useless requests.</li>
+                    <li><strong>Timeout</strong> - Blanket term for a player being a general nuisance in chat; spamming players; raging, etc.</li>
+                    <li><strong>Discrimination</strong> - Blanket term for a player discriminating against other players for arbitrary things, i.e. their race, sex, etc.</li>
+                </ul>
+                <br/>
+                <h1>&nbsp;⚠️ In-game Warns</h1>
+                <h2>&nbsp;&nbsp;&nbsp;The following can get you warned:</h2>
+                <ul>
+                    <li><strong>Prebuilding</strong> - Utilized in SkyHigh games, when a player's base is too close (about 20 blocks) to another's.</li>
+                    <li><strong>Groundcamping</strong> - Utilized in SkyHigh games, when its disallowed, a player is not disallowed to go down to camp (stay down to wait for other players).</li>
+                    <li><strong>Skybasing/Towering</strong> - Towering even after it's Meetup, typically only warnable when it's disallowed.</li>
+                    <li><strong>Go to 0,0, Mining at Meetup or Go to Surface</strong> - Not going to 0,0, taking a long time to get to 0,0 because of enchanting, shearing, mining, etc.</li>
+                    <li><strong>Running at Meetup</strong> - Blanket term for people that run away from multiple fights at meetup, this rule is typically loose depending on the host.</li>
+                    <li><strong>Teaming in Arena</strong> - Crossteaming with other players in the FFA Arena.</li>
+                </ul>
+                <br/>
+            </main>
+        </>
+    )
+}
